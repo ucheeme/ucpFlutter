b202f0f15ec828f3604ba7f0f83c033142274be6
ucheeme <ucheemekavictor@gmail.com>
2024-12-11 22:50:45 +0100

    signUp flow 2

---
+++ lib/app/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const String baseUrlStaging =
+      'https://solutions.cooplatform.com.ng/UCPMemberRevApi/api/';
+}
